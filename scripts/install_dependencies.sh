ec22e8fa85e40bd7be4f51061b07c826bd054ad9
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2025-07-04 09:02:04 +0000

    After dav review

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
 
+# Install gcloud CLI
+apt-get update && apt-get install -y curl unzip gnupg
+
+echo "Installing gcloud CLI..."
+curl -sSL https://sdk.cloud.google.com | bash
+exec -l $SHELL
+gcloud init
+
+# Install Python dependencies
 pip3 install --user -r requirements.txt
+
+# Set up dbt folder
 mkdir -p ~/.dbt/
-cd jaffle_shop_training
-echo "Hi! Start by executing 'dbt init' to create your first dbt project."
