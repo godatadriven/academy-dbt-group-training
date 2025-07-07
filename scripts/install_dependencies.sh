555b6d82b55ce032845d3e85a76f88006aed7e3f
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2025-07-07 09:40:07 +0000

    remove g-init

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-# Install gcloud CLI
-apt-get update && apt-get install -y curl unzip gnupg
-
-echo "Installing gcloud CLI..."
-curl -sSL https://sdk.cloud.google.com | bash
-exec -l $SHELL
-gcloud init
+set -e  # Exit immediately if a command exits with a non-zero status
 
 # Install Python dependencies
 pip3 install --user -r requirements.txt
