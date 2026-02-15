fe6bb0f7a79e646c7e5951b95f9bfa91f2035d1a
Lucy Sheppard <50021249+lushep@users.noreply.github.com>
2026-02-15 20:20:54 +0000

    Remove jaffle

--- jaffle_shop/analyses/scratchpad.sql
+++
@@ -1,5 +0,0 @@
-/*
-dbt run-operation generate_source --args '{"schema_name": "data_warehousing", "generate_columns": true}'
-
-dbt run-operation generate_model_yaml --args '{"model_names": ["dim_customers"]}'
-*/
