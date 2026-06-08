3d6ab3781388961191514082194413da88c98e05
Max Poirault <max.poirault@xebia.com>
2026-06-08 10:52:38 +0200

    Add day 2 exercise solutions: staging layer, sources, tests, docs
    - Reorganise models into staging/ and marts/ (remove example/)
    - Rename customers.sql to marts/dim_customers.sql, refactor first two
      CTEs into stg_jaffle_shop__customers and stg_jaffle_shop__orders
    - Materialize all models as table, staging models as view
    - Add _sources.yml with source definitions, primary key tests, and
      freshness on orders (_etl_loaded_at)
    - Add _stg_jaffle_shop_models.yml with unique/not_null, relationships,
      and accepted_values tests
    - Add a customer_id docs block and link it via doc()

--- jaffle_shop/models/marts/dim_customers.sql
+++ jaffle_shop/models/marts/dim_customers.sql
@@ -1,21 +1,12 @@
 with customers as (
 
-select 
-    id as customer_id,
-    first_name,
-    last_name
-from raw_jaffle_shop.customers
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-select 
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status as order_status
-from raw_jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -34,7 +25,6 @@ customer_orders as (
 
 ),
 
-
 final as (
 
     select
@@ -47,8 +37,8 @@ final as (
 
     from customers
 
-    left join customer_orders on 
-    customers.customer_id = customer_orders.customer_id
+    left join customer_orders
+        on customers.customer_id = customer_orders.customer_id
 
 )
 
