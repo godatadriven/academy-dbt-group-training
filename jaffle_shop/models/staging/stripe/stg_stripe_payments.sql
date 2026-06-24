745994a4c395e9b92b498102bdc8b0a1cb2730f5
elvbergen2906 <e.vanbergen@erasmusmc.nl>
2026-06-24 13:28:28 +0000

    transform cents

--- jaffle_shop/models/staging/stripe/stg_stripe_payments.sql
+++ jaffle_shop/models/staging/stripe/stg_stripe_payments.sql
@@ -6,7 +6,7 @@ with source as (
           {{ adapter.quote("payment_id") }},
         {{ adapter.quote("order_id") }},
         {{ adapter.quote("payment_method") }},
-        {{ adapter.quote("payment_amount") }},
+        {{ adapter.quote("payment_amount") }} / 100 as payment_amount,
         {{ adapter.quote("payment_date") }}
 
       from source
